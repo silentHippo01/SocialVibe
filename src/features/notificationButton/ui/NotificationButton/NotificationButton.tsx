0a8d8e3c1a48f87b73ed6b63cc7a353c6f3da97c
Andrew <110103494+silentHippo01@users.noreply.github.com>
2023-08-19 19:09:09 +0300

    add drawer, add device detector

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,11 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './NotificationButton.module.scss';
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Icon } from 'shared/ui/Icon/Icon';
 import { NotificationList } from 'entities/Notification';
 import { Popover } from 'shared/ui/Popup';
 import Notification from 'shared/assets/icon/Notification-20-20.svg';
+import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 interface NotificationButtonProps {
     className?: string;
@@ -14,18 +15,37 @@ interface NotificationButtonProps {
 export const NotificationButton = memo((props: NotificationButtonProps) => {
     const { className } = props;
 
+    const [isOpen, setIsOpen] = useState(false);
+    const onOpenDrawer = useCallback(() => {
+        setIsOpen(true);
+    }, [])
+
+    const onCloseDrawer = useCallback(() => {
+        setIsOpen(false);
+    }, [])
+
+    const trigger = (
+        <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
+            <Icon Svg={Notification} inverted />
+        </Button>
+    )
+
     return (
-        <>
-            <Popover
-                className={cls.NotificationButton}
-                direction={'bottom left'}
-                trigger={(
-                    <Button theme={ButtonTheme.CLEAR}>
-                        <Icon Svg={Notification} inverted />
-                    </Button>
-                )}>
-                <NotificationList className={cls.notifications} />
-            </Popover>
-        </>
+        <div>
+            <BrowserView>
+                <Popover
+                    className={cls.NotificationButton}
+                    direction={'bottom left'}
+                    trigger={trigger}>
+                    <NotificationList className={cls.notifications} />
+                </Popover>
+            </BrowserView>
+            <MobileView>
+                {trigger}
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer} >
+                    <NotificationList />
+                </Drawer >
+            </MobileView>
+        </div>
     );
 });
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -12,6 +12,8 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { HStack } from 'shared/ui/Stack';
 import { NotificationButton } from 'features/notificationButton';
 import { AvatarDropdown } from 'features/avatarDropdown';
+import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { NotificationList } from 'entities/Notification';
 
 interface NavBarProps {
     className?: string;
